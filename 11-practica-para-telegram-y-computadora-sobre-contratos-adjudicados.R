bdf64eb65ae2b68ce6c881acef4a431933a41cad
Cesar Gerardo Hernández Vargas <66124391+cghv94@users.noreply.github.com>
2020-09-30 08:49:26 -0500

    Rename Práctica para telegram y computadora sobre contratos adjudicados.R to 11-practica-para-telegram-y-computadora-sobre-contratos-adjudicados.R

--- 11-practica-para-telegram-y-computadora-sobre-contratos-adjudicados.R
+++ 11-practica-para-telegram-y-computadora-sobre-contratos-adjudicados.R
@@ -1,9 +1,9 @@
-# UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO
-# Facultad de Economía
-# Estadística 2020-2
-# Profesor: Cesar Hernández
-# Práctica para telegram y computadora (no se necesita instalar nungún paquete)
-# Análisis estadístico sobre contratos adjudicados por el Gobuerno de la 
+# UNIVERSIDAD NACIONAL AUTÃ“NOMA DE MÃ‰XICO
+# Facultad de EconomÃ­a
+# EstadÃ­stica 2020-2
+# Profesor: Cesar HernÃ¡ndez
+# PrÃ¡ctica para telegram y computadora (no se necesita instalar nungÃºn paquete)
+# AnÃ¡lisis estadÃ­stico sobre contratos adjudicados por el Gobuerno de la 
 # CDMX durante la pandemia de Covid-19 en R
 
 # Importando datos
@@ -12,49 +12,49 @@ adjudicaciones <- read.csv("https://datos.cdmx.gob.mx/explore/dataset/contratos-
 # Nombre de las columnas
 colnames(adjudicaciones)
 
-# Resumen estadístico
+# Resumen estadÃ­stico
 summary(adjudicaciones$monto_contrato)
 
-# Tabla de número de adjudicaciones según el ente
+# Tabla de nÃºmero de adjudicaciones segÃºn el ente
 ente <- adjudicaciones$ente_nombre
 frecuencia_ente <- data.frame(sort(table(ente)))
 frecuencia_ente <- transform(frecuencia_ente, FreqAc = cumsum(Freq), 
                              FreqRel = prop.table(Freq),
                              FreqRelAc = cumsum(prop.table(Freq)))
 
-# Gráfica de número de adjudicaciones según el ente
+# GrÃ¡fica de nÃºmero de adjudicaciones segÃºn el ente
 plot(frecuencia_ente$ente,frecuencia_ente$Freq,
-     ylab = "Monto de la adjudicación",
+     ylab = "Monto de la adjudicaciÃ³n",
      las = 2,
      cex.axis = 0.4)
 
-# Tabla de número de adjudicaciones según el proveedor
+# Tabla de nÃºmero de adjudicaciones segÃºn el proveedor
 proveedor <- adjudicaciones$proveedor_nombre
 frecuencia_proveedor <- data.frame(sort(table(proveedor)))
 frecuencia_proveedor <- transform(frecuencia_proveedor, FreqAc = cumsum(Freq), 
                                   FreqRel = prop.table(Freq),
                                   FreqRelAc = cumsum(prop.table(Freq)))
 
-# Gráfica de número de adjudicaciones según el proveedor
+# GrÃ¡fica de nÃºmero de adjudicaciones segÃºn el proveedor
 plot(frecuencia_proveedor$proveedor,frecuencia_proveedor$Freq,
-     ylab = "Monto de la adjudicación",
+     ylab = "Monto de la adjudicaciÃ³n",
      las = 2,
      cex.axis = 0.35)
 
-# Tabla de número de adjudicaciones según el la modalidad
+# Tabla de nÃºmero de adjudicaciones segÃºn el la modalidad
 modalidad <- adjudicaciones$modalidad_contratacion
 frecuencia_modalidad <- data.frame(sort(table(modalidad)))
 frecuencia_modalidad <- transform(frecuencia_modalidad, FreqAc = cumsum(Freq), 
                                   FreqRel = prop.table(Freq),
                                   FreqRelAc = cumsum(prop.table(Freq)))
 
-# Monto de la adjudicación según el proveedor  
+# Monto de la adjudicaciÃ³n segÃºn el proveedor  
 plot(adjudicaciones$proveedor_nombre,adjudicaciones$monto_contrato,
-     ylab = "Monto de la adjudicación",
+     ylab = "Monto de la adjudicaciÃ³n",
      las = 2,
      cex.axis = 0.4)
 
-# Análisis de los montos de las adjudicaciones
+# AnÃ¡lisis de los montos de las adjudicaciones
 monto <- adjudicaciones$monto_contrato
 
 # Histograma de los montos de las adjudicaciones
@@ -66,7 +66,7 @@ monto_clases <- transform(monto_clases, FreqAc = cumsum(Freq),
                           FreqRel = prop.table(Freq),
                           FreqRelAc = cumsum(prop.table(Freq)))
 
-# Resumen estadístico de los montos de las adjudicaciones
-resumen <- data.frame(Mínimo = min(monto), Máximo = max(monto), Mediana = median(monto),
-                      Media = mean(monto), Desviación = sd(monto), Varianza = var(monto),
-                      Asimetría = skewness(monto), Curtosis = kurtosis(monto))
+# Resumen estadÃ­stico de los montos de las adjudicaciones
+resumen <- data.frame(MÃ­nimo = min(monto), MÃ¡ximo = max(monto), Mediana = median(monto),
+                      Media = mean(monto), DesviaciÃ³n = sd(monto), Varianza = var(monto),
+                      AsimetrÃ­a = skewness(monto), Curtosis = kurtosis(monto))
